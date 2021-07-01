fdaa3bb77e36b88e360cbf3a95f24b9dfd25789c
joao <joao.vitoramc@gmail.com>
2021-07-01 12:54:25 -0300

    Serviços básicos + endpoints de user (signup e login)

---
+++ src/business/UserBusiness.ts
@@ -0,0 +1,82 @@
+import userDataBase, { UserDataBase } from "../data/UserDataBase";
+import authenticator, { Authenticator } from "../services/Authenticator";
+import hashManager, { HashManager } from "../services/HashManager";
+import idGenerator, { IdGenerator } from "../services/IdGenerator";
+import { LoginInputDTO, UserInputDTO } from "../model/User";
+import { CustomError } from "../error/CustomError";
+
+export class UserBusiness {
+
+    constructor(
+        private userDataBase: UserDataBase,
+        private authenticator: Authenticator,
+        private hashManager: HashManager,
+        private idGenerator: IdGenerator
+    ) {}
+
+
+    async createUser(user: UserInputDTO){
+        try {
+            if(!user){
+                throw new CustomError(422, "Faltam informações!")
+            }
+
+            if (user.email.indexOf("@") === -1) {
+                throw new CustomError(422, "Email inválido");
+            }
+    
+             if (user.password.length < 6) {
+                throw new CustomError(422, "Senha inválida");
+            }
+
+            const id = this.idGenerator.generate()
+    
+            const hashPassword = await this.hashManager.hash(user.password)
+    
+            await this.userDataBase.createUser(id, user.email, user.name, user.nickname, hashPassword)
+    
+            const accessToken = this.authenticator.generateToken({ id })
+    
+            return accessToken
+        } catch (error) {
+            if (error.message.includes("key 'email'")) {
+                throw new CustomError(409, "Esse email já está sendo usado")
+             }
+             throw new CustomError(error.statusCode, error.message)
+        }
+
+    }
+
+    public async login(loginData: LoginInputDTO) {
+        try {
+            if (!loginData.email || !loginData.password) {
+                throw new CustomError(422, "Informações faltando");
+             }
+    
+             const user = await this.userDataBase.getUserByEmail(loginData.email);
+    
+             if (!user) {
+                throw new CustomError(401, "Credenciais inválidas");
+             }
+
+             const isPasswordCorrect = await this.hashManager.compare(loginData.password, user.getPassword())
+
+             if (!isPasswordCorrect) {
+                throw new CustomError(401, "Senha incorreta");
+             }
+             
+            const accessToken = this.authenticator.generateToken({id: user.getId()})
+
+            return accessToken
+        } catch (error) {
+            throw new CustomError(error.statusCode, error.message)
+        }
+    }
+}
+
+export default new UserBusiness(
+    userDataBase,
+    authenticator,
+    hashManager,
+    idGenerator
+)
---
+++ src/data/UserDataBase.ts
@@ -0,0 +1,58 @@
+import { User } from "../model/User";
+import { BaseDatabase } from "./BaseDataBase";
+
+export class UserDataBase extends BaseDatabase {
+    private static TABLE_NAME = ""
+
+    public async createUser(
+        id: string,
+        email: string,
+        name: string,
+        nickname: string,
+        password: string
+    ): Promise<any> {
+        try {
+            await this.getConnection()
+            .insert({
+                id,
+                email,
+                name,
+                nickname,
+                password
+            })
+            .into(UserDataBase.TABLE_NAME)
+        } catch (error) {
+            throw new Error(error.sqlMessage || error.message);
+        }
+    }
+
+    public async getUserById(
+        id: string
+    ) : Promise<User | undefined> {
+        try {
+          const result = await this.getConnection()
+            .select("*")
+            .from(UserDataBase.TABLE_NAME)
+            .where({id})
+
+            return result[0];
+        } catch (error) {
+            throw new Error(error.sqlMessage || error.message)
+        }
+    }
+
+    public async getUserByEmail(email: string): Promise<User | undefined> {
+        try {
+            const result = await this.getConnection()
+            .select("*")
+            .from(UserDataBase.TABLE_NAME)
+            .where({email})
+
+            return result[0]
+        } catch (error) {
+            throw new Error(error.sqlMessage || error.message)
+        }
+    }
+}
+
+export default new UserDataBase()
