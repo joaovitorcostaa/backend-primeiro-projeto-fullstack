18e259da318e5c9f9e219fa60ed298274a5437f7
joao <joao.vitoramc@gmail.com>
2021-07-18 18:52:53 -0300

    erro no deploy

--- src/controller/app.ts
+++ src/controller/app.ts
@@ -8,11 +8,13 @@ export const app = express()
 
 app.use(express.json())
 
-const server = app.listen(process.env.DB_PORT || 3003, () => {
-    if (server) {
-      const address = server.address() as AddressInfo;
-      console.log(`Servidor rodando em http://localhost:${address.port}`);
-    } else {
-      console.error(`Falha ao rodar o servidor.`);
-    }
-  });
+const PORT = process.env.DB_PORT || 3003;
+
+const server = app.listen(PORT, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Servidor rodando em http://localhost:${address.port}`);
+  } else {
+    console.error(`Falha ao rodar o servidor.`);
+  }
+});
